f4c0323cca1bb7e758464fd93c39a86fde9ba6f6
Chris Koorzen <fckoorzen@gmail.com>
2024-08-27 19:20:32 +0200

    Expand regex and add more explanatory comments.

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 function validateSyntax() {
-    let input = document.getElementById('petInput').value;
+    let input = document.getElementById('petInput').value.trim();   // Get input and clear whitespace
     
     // Check if input starts with 'pet_' and followed by alphanumeric characters
     // Regex:
+    //              ^   =  match only from start of input or string
+    //           pet_   =  match the characters 'pet_' exactly and in that order
     //          \d{4}   =  match 4 digits exactly
-    //      [^\s\d\W_]+ =  match anything except whitespace, digits, non-word characters and underscore (the alphabet only by exclusion)
-    //                     it will discard any input following an "illegal" character
+    //      [^\s\d\W_]  =  match anything except whitespace (\s), digits (\d), non-word characters (\W) and underscore
+    //                     (the alphabet only by exclusion)
+    //                     equivalent to [a-zA-Z]
+    //                     it will not match any input following an "illegal" character
+    //              +   =  match preceding specifier atleast once
+    //                     is "greedy", which means it will continue to match as long as specifier is true
+    //                     equivalent to {1, }
     
-    const petRegex = /\d{4}[^\s\d\W_]+/;
+    const petRegex = /^pet_\d{4}[^\s\d\W_]+/;
 
-    let requiredIdentifier = input.slice(0, 4);
-    let petDetails = input.slice(4);
-    let match = petDetails.match(petRegex); // will return null if no match is found
+    let match = input.match(petRegex); // will return null if no match is found
     let status, message = "";
-
-    if (requiredIdentifier==='pet_' && !(match===null)){
+    
+    if ( !(match===null) ){
         status = "success";
-        message = "Your pet's name is: " + match[0].slice(4);
+        message = "Your pet's name is: " + match[0].slice(8);
 
 
         // Do advanced validation eg.
         // Birth Year cannot be greater than current year
         let date = new Date();
-        if (Number(match[0].slice(0, 4)) > date.getFullYear()){
+        if (Number(match[0].slice(4, 8)) > date.getFullYear()){
             status = "fail";
             message = "Birth Year cannot be greater than current year";
         }
         // Pet name must be minimum length
-        if (match[0].slice(4).length < 3){
+        if (match[0].slice(8).length < 3){
             status = "fail";
             message = "Pet name must be at least 3 characters";
         }
